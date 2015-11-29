a804cfcfab79e5b1f989796e331f2b30570ccdaa
AD\eliot2 <ecsit39@gmail.com>
2015-11-29 06:34:29 -0500

    Almost got item collection working. OBJs are offcenter -_-

---
+++ Assets/Code/score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class score : MonoBehaviour
+{
+    public scoreDisplay myScore;
+    public int scoreGoalNumber;//this is what team you're on  -kit
+    public int[] ScoreForYou = new int[4];//4 players, hardcoded -kit
+    public int scoreToWin;//This isn't given a value by anything I've coded -Kit
+
+    void OnTriggerEnter (Collider col)
+    {
+        if(col.gameObject.name.Contains("TV"))//when there's a collision -kit
+        {
+            Destroy(col.gameObject);
+            ScoreForYou[scoreGoalNumber] += 1;
+            myScore.ScoreText();
+            if (ScoreForYou[scoreGoalNumber] >= scoreToWin)//Might be better as == scoreToWin idk -kit
+            {
+                myScore.Winner();
+                Debug.Log("you win!!!!!" +scoreGoalNumber);
+            }
+        }
+    }
+}
---
+++ Assets/Code/scoreDisplay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scoreDisplay : MonoBehaviour {
+
+    public Text MyPrintThing;//IMPORTANT!!!!!!! Name the text ui "Text"
+    public score Player;
+
+    // Use this for initialization
+    void Start () {
+
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+    public void ScoreText()
+    {
+        MyPrintThing.text = "PLAYER" +Player.scoreGoalNumber +"SCORES";
+    }
+    public void Winner()
+    {
+        MyPrintThing.text = "PLAYER" +Player.scoreGoalNumber +"WINS";
+    }
+}
