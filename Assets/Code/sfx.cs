c10dd336df06f83318aa0e67ecb437109bc34b5b
ecsit <ecsit39@gmail.com>
2015-11-30 04:34:56 -0500

    'eyyy its on windows or something

---
+++ Assets/Code/sfx.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sfx : MonoBehaviour {
+
+    public AudioSource myPlayer;
+    public AudioClip[] musicList;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlaySong(int song)
+    {
+        myPlayer.Stop();
+        myPlayer.clip = musicList[song];
+        myPlayer.Play();
+    }
+}
